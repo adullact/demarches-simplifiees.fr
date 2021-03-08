8e70c44da92928322b3771eb89e5b9adeb06654d
François Vantomme <fvantomme@synbioz.com>
2021-03-08 13:56:13 +0100

    Test (Universign): skip test if API URL is missing
    Addressable::URI::InvalidURIError:
     Absolute URI missing hierarchical segment: 'http://'

--- spec/lib/universign/api_spec.rb
+++ spec/lib/universign/api_spec.rb
@@ -4,6 +4,9 @@ describe Universign::API do
 
     let(:digest) { Digest::SHA256.hexdigest("CECI EST UN HASH") }
 
-    it { is_expected.not_to be_nil }
+    it do
+      skip "L'URL de l'API Universign est obligatoire pour effectuer ce test" if UNIVERSIGN_API_URL.blank?
+      is_expected.not_to be_nil
+    end
   end
 end
