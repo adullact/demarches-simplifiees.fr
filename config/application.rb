122912b8f479504d58b50aa97e31452d82dfe1e8
François Vantomme <fvantomme@synbioz.com>
2021-06-11 16:33:55 +0200

    refactor(FranceConnect): use Rails custom configuration

--- config/application.rb
+++ config/application.rb
@@ -86,6 +86,10 @@ module TPS
     # @see https://guides.rubyonrails.org/configuring.html#custom-configuration
     config.x.clamav.enabled = ENV.fetch("CLAMAV_ENABLED", "enabled") == "enabled"
 
+    config.x.france_connect.enabled = ENV.fetch("FRANCE_CONNECT_ENABLED", "enabled") == "enabled"
+    config.x.france_connect.particulier = config_for(:france_connect)
+    config.x.fcp = config.x.france_connect.particulier
+
     config.view_component.generate_sidecar = true
     config.view_component.generate_locale = true
     config.view_component.generate_distinct_locale_files = true
