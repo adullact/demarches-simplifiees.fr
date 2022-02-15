c4b6510e5422ac7caf2158d3a0b33ca90b1884e2
François Vantomme <fvantomme@synbioz.com>
2022-02-15 14:39:24 +0100

    refactor(FranceConnect): prefer feature flag over env var

--- app/helpers/france_connect_helper.rb
+++ app/helpers/france_connect_helper.rb
@@ -1,6 +1,6 @@
 module FranceConnectHelper
   def france_connect_enabled?(procedure: nil)
-    return false if !FranceConnectService.enabled?
+    return false if !Flipper.enabled?(:france_connect, current_user)
 
     fcp_secrets = Rails.application.secrets.france_connect_particulier
 
