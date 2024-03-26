c84cfe64445a76f08743066223be736fbb9174ef
Martin Catty <martin.catty@ouidou.fr>
2024-03-26 13:42:06 +0000

    feat: allow scaleway in CSP
    As we use direct upload we need to allow it.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -24,6 +24,7 @@ Rails.application.config.content_security_policy do |policy|
   connect_whitelist << ENV.fetch('APP_HOST')
   connect_whitelist << ENV.fetch('APP_HOST_LEGACY') if ENV.key?('APP_HOST_LEGACY')
   connect_whitelist << "*.amazonaws.com" if Rails.configuration.active_storage.service == :amazon
+  connect_whitelist << "s3.fr-par.scw.cloud" if Rails.configuration.active_storage.service == :scaleway
   connect_whitelist += [URI(ENV["SENTRY_DSN_JS"]).host, URI(ENV["SENTRY_DSN_RAILS"]).host].compact.uniq
   connect_whitelist << URI(DS_PROXY_URL).host if DS_PROXY_URL.present?
   connect_whitelist << URI(API_ADRESSE_URL).host if API_ADRESSE_URL.present?
