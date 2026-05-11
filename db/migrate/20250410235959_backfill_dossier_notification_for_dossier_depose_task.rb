f3fcc3da7877c83a3578b92d77c324cfb87ad831
Pierre de La Morinerie <pierre@codeureusesenliberte.fr>
2026-05-11 13:41:17 +0200

    db: don't run dossier_notification maintenance task
    It fills a column for groupe_instructeur, but since then DossierNotification operates on instructeur instead.
    
    The backfill doesn't make much sense then.

--- db/migrate/20250410235959_backfill_dossier_notification_for_dossier_depose_task.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class BackfillDossierNotificationForDossierDeposeTask < ActiveRecord::Migration[7.2]
-  disable_ddl_transaction!
-
-  def up
-    MaintenanceTasks::Runner.run_sync(name: "Maintenance::T20250410backfillDossierNotificationForDossierDeposeTask")
-  end
-end
