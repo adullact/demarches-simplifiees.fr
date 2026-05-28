e5ca051c9b344dc6daa0708f9d19e7ee715cb424
Pierre de La Morinerie <pierre@codeureusesenliberte.fr>
2026-05-28 10:12:58 +0200

    tasks: restore procedure path migration
    The task was deleted in https://github.com/demarche-numerique/demarche.numerique.gouv.fr/pull/11337

---
+++ app/tasks/maintenance/migrate_existing_procedure_paths_task.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Maintenance
+  class MigrateExistingProcedurePathsTask < MaintenanceTasks::Task
+    include RunnableOnDeployConcern
+
+    run_on_first_deploy
+
+    # Local model, for allowing access to the now-ignored `path` column
+    class Procedure < ::Procedure
+      self.ignored_columns -= [:path.to_s]
+    end
+
+    def collection
+      Procedure.all
+    end
+
+    def process(element)
+      element.ensure_path_exists
+      element.save!(validate: false)
+
+      if element.publiee?
+        element.procedure_paths << ProcedurePath.find_or_create_by(path: element[:path])
+      else
+        if !Procedure.publiees.exists?(path: element[:path]) # the path is not used by another published procedure
+          element.procedure_paths << ProcedurePath.find_or_create_by(path: element[:path])
+        end
+      end
+      element.save!(validate: false)
+    end
+  end
+end
