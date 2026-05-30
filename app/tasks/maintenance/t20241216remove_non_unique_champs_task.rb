bb56635afbd7e888720e10de850c0d12fe4375ce
Pierre de La Morinerie <pierre@codeureusesenliberte.fr>
2026-05-30 14:52:28 +0200

    tasks: optimize remove_non_unique_champs_task

--- app/tasks/maintenance/t20241216remove_non_unique_champs_task.rb
+++ app/tasks/maintenance/t20241216remove_non_unique_champs_task.rb
@@ -7,22 +7,31 @@ module Maintenance
     include RunnableOnDeployConcern
     include StatementsHelpersConcern
 
+    collection_batch_size(1000)
+
     def collection
-      Dossier.includes(champs: [])
+      Dossier.all.select(:id)
     end
 
     def process(dossier)
-      duplicated_champ_ids = dossier.champs.where(row_id: [Champ::NULL_ROW_ID, nil])
+      rowless_champs = dossier.champs
+        .where(row_id: [Champ::NULL_ROW_ID, nil])
         .order(id: :desc)
         .select(:id, :stream, :stable_id, :row_id)
+
+      duplicated_champ_ids = rowless_champs
         .group_by { "#{_1.stream}-#{_1.public_id}" }
         .values
         .flat_map { _1[1..].map(&:id) }
+      nil_row_champ_ids = rowless_champs.filter { _1[:row_id].nil? }.map(&:id)
+
       Dossier.transaction do
         if duplicated_champ_ids.present?
           Dossier.no_touching { dossier.champs.where(id: duplicated_champ_ids).destroy_all }
         end
-        dossier.champs.where(row_id: nil).update_all(row_id: Champ::NULL_ROW_ID)
+        if nil_row_champ_ids.present?
+          dossier.champs.where(id: nil_row_champ_ids).update_all(row_id: Champ::NULL_ROW_ID)
+        end
       end
     end
   end
