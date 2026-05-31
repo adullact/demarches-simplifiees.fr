77495c237b8635dbfddcbccbe5076e60e55edcf1
Pierre de La Morinerie <pierre@codeureusesenliberte.fr>
2026-05-31 18:51:03 +0200

    tasks: optimize nullify_row_id_task

--- app/tasks/maintenance/t20250526_nullify_row_id_task.rb
+++ app/tasks/maintenance/t20250526_nullify_row_id_task.rb
@@ -12,28 +12,45 @@ module Maintenance
     # run_on_first_deploy
 
     def collection
-      Dossier.all
+      Dossier.select(:id).in_batches(of: 100)
     end
 
-    def process(dossier)
-      with_nil_row_id, with_null_row_id = dossier.champs
+    def process(dossiers_batch)
+      Champ
+        .where(dossier_id: dossiers_batch.map(&:id))
         .where(row_id: [nil, Champ::NULL_ROW_ID])
-        .pluck(:row_id, :stream, :stable_id, :id, :updated_at)
+        .pluck(:row_id, :stream, :stable_id, :id, :updated_at, :dossier_id)
+        .group_by { |(_row_id, _stream, _stable_id, _id, _updated_at, dossier_id)| dossier_id }
+        .each do |dossier_id, champs|
+          process_dossier(dossier_id, champs)
+        end
+    end
+
+    def process_dossier(dossier_id, champs)
+      with_nil_row_id, with_null_row_id = champs
         .partition { _1.first == nil }
         .map { _1.index_by { |(_, stream, stable_id)| [stream, stable_id] } }
 
+      to_destroy_ids = []
+      to_nullify_ids = []
+
       with_null_row_id.values.each do |(_, stream, stable_id, id, updated_at)|
         if with_nil_row_id[[stream, stable_id]].present?
-          with_nil_updated_at, with_nil_id = with_nil_row_id[[stream, stable_id]].reverse
+          with_nil_id, with_nil_updated_at = with_nil_row_id[[stream, stable_id]].then { |(_row_id, _stream, _stable_id, id, updated_at)| [id, updated_at] }
           if with_nil_updated_at > updated_at
-            dossier.champs.where(id: id).destroy_all
+            to_destroy_ids << id
           else
-            dossier.champs.where(id: with_nil_id).destroy_all
-            dossier.champs.where(id:).update_all(row_id: nil)
+            to_destroy_ids << with_nil_id
+            to_nullify_ids << id
           end
         else
-          dossier.champs.where(id:).update_all(row_id: nil)
+          to_nullify_ids << id
         end
+      end
+
+      Dossier.no_touching do
+        Champ.where(dossier_id:).where(id: to_destroy_ids).destroy_all unless to_destroy_ids.empty?
+        Champ.where(dossier_id:).where(id: to_nullify_ids).update_all(row_id: nil) unless to_nullify_ids.empty?
       end
     end
 
--- spec/tasks/maintenance/t20250526_nullify_row_id_task_spec.rb
+++ spec/tasks/maintenance/t20250526_nullify_row_id_task_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 module Maintenance
   RSpec.describe T20250526NullifyRowIdTask do
     describe "#process" do
-      subject(:process) { described_class.process(dossier) }
+      subject(:process) { described_class.process(Dossier.select(:id).where(id: dossier.id).in_batches.first) }
       let(:procedure) { create(:procedure, types_de_champ_public: [{}, { type: :repetition, children: [{ type: :text }] }]) }
       let(:dossier) { create(:dossier, :with_populated_champs, procedure:) }
       let(:champs_with_null_row_id) { dossier.champs.where(row_id: [nil, Champ::NULL_ROW_ID]) }
@@ -25,12 +25,31 @@ module Maintenance
       end
 
       context 'deal with conflicts' do
-        before do
-          attributes = dossier.champs.where(row_id: Champ::NULL_ROW_ID).first.attributes
-          dossier.champs.create(attributes.merge(row_id: nil, id: nil))
+        let(:with_null_row_id) { dossier.champs.where(row_id: Champ::NULL_ROW_ID).first }
+        let(:with_nil_row_id) { dossier.champs.create(with_null_row_id.attributes.merge(row_id: nil, id: nil)) }
+
+        context 'when the champ with row_id: nil has been updated last' do
+          before { [with_null_row_id, with_nil_row_id].each(&:touch) }
+
+          it 'destroys the NULL_ROW_ID champ' do
+            expect { process }. to change { Champ.where(id: with_null_row_id).to_a }.from([with_null_row_id]).to([])
+          end
+
+          it 'nullify row_id' do
+            expect { process }. to change { null_row_id_counts }.from([1, 1]).to([1, 0])
+          end
         end
-        it 'nullify row_id' do
-          expect { process }. to change { null_row_id_counts }.from([1, 1]).to([1, 0])
+
+        context 'when the champ with row_id: NULL_ROW_ID has been updated last' do
+          before { [with_nil_row_id, with_null_row_id].each(&:touch) }
+
+          it 'destroys the row_id: nil champ' do
+            expect { process }. to change { Champ.where(id: with_nil_row_id).to_a }.from([with_nil_row_id]).to([])
+          end
+
+          it 'nullify row_id' do
+            expect { process }. to change { null_row_id_counts }.from([1, 1]).to([1, 0])
+          end
         end
       end
     end
